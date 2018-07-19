fc3e4a9d1458c85a9d493cc6cbc70f78e2d85e52
msokalski <msokalski@users.noreply.github.com>
2018-07-19 14:58:44 +0200

    Update delabella.h

--- delabella.h
+++ delabella.h
@@ -1,8 +1,8 @@
 #ifndef DELABELLA_H
 #define DELABELLA_H
 
-// returns: positive value: number triangle indices, negative: number of line segment indices (degenerated input)
-//          triangle indices in abc array are always returned in CCW order
+// returns: positive value: number of triangle indices, negative: number of line segment indices (degenerated input)
+//          triangle indices in abc array are always returned in clockwise order
 
 int DelaBella(int points, const double* xy/*[points][2]*/, int* abc/*[2*points-5][3]*/);
 
