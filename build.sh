f5e20b98ea3265a603909559668fd430c3f7b0ab
Marcin Sokalski <msokalski@novomatic-tech.com>
2022-08-25 01:46:58 +0200

    .

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ CPP=g++
 # fp-math must conform to strict IEEE behaviour, ie: don't use -ffast-math
 # adding -I for clang
 OPT="-std=c++17 -O3 -I/usr/include/SDL2"
+LNK=""
+
+if [ -d "fade" ]; then
+    OPT="$OPT -DWITH_FADE"
+    LNK="-Lfade -lfade2d -Wl,-rpath=fade"
+fi
 
 if [ -d "delaunator-cpp" ]; then
     OPT="$OPT -DWITH_DELAUNATOR"
@@ -17,4 +23,4 @@ if [[ $OSTYPE == 'darwin'* ]]; then
     OPT="$OPT -framework OpenGL"
 fi    
 
-$CPP $OPT delabella.cpp delabella-sdl2.cpp -lSDL2 -lGL -o delabella-sdl2
+$CPP $OPT delabella.cpp delabella-sdl2.cpp $LNK -lSDL2 -lGL -o delabella-sdl2
