e7b4673a2da1ebd64918e14459f3c36027c5e43e
msokalski <msokalski@novomatic-tech.com>
2023-07-10 17:01:53 +0200

    added svg output

--- batch_dba_test.sh
+++ batch_dba_test.sh
@@ -1,4 +1,4 @@
 for V in {0..61}
 do
-  ./dba_test tests/dlbtest_$V.txt tests/dlbtest_$V.log
+  ./dba_test tests/dlbtest_$V.txt tests/dlbtest_$V.log tests/dlbtest_$V.svg
 done
--- dba_test.cpp
+++ dba_test.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstdarg>
+#include <cmath> // INFINITY
 
 #include "delabella.h"
 
@@ -80,6 +81,50 @@ int main( int argc, char* argv[] )
     }
     printf( "\n" );
 
+    FILE* svg = 0;
+    double svg_scale = 1;
+    double svg_trans_x = 0;
+    double svg_trans_y = 0;
+
+    if (argc>3)    
+        svg = fopen(argv[3],"w");
+
+    if (svg)
+    {
+        double x_min = INFINITY, x_max = -INFINITY;
+        double y_min = INFINITY, y_max = -INFINITY;
+        for ( int j = 0; j < npt; j++ )
+        {
+            if (cloud[j].x < x_min)
+                x_min = cloud[j].x;
+            if (cloud[j].x > x_max)
+                x_max = cloud[j].x;
+            if (cloud[j].y < y_min)
+                y_min = cloud[j].y;
+            if (cloud[j].y > y_max)
+                y_max = cloud[j].y;
+        }
+
+        double x_size = x_max - x_min;
+        double y_size = y_max - y_min;
+        double in_size = x_size > y_size ? x_size : y_size;
+        double out_size = 640;
+
+        svg_scale = out_size / (in_size * 1.2);
+        svg_trans_x = out_size * 0.5 - (x_max + x_min) * svg_scale * 0.5;
+        svg_trans_y = out_size * 0.5 - (y_max + y_min) * svg_scale * 0.5;
+
+        char svg_size[200];
+        sprintf(svg_size,"width=\"%f\" height=\"%f\" viewBox=\"%f %f %f %f\"", out_size, out_size, 0.0, 0.0, out_size, out_size);
+        const char* svg_attr = "fill=\"none\" stroke=\"currentcolor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\"";
+
+        fprintf(svg, "<svg xmlns=\"http://www.w3.org/2000/svg\" %s %s>\n", svg_size, svg_attr);
+
+        fprintf(svg, "  <rect x=\"%f\" y=\"%f\" width=\"%f\" height=\"%f\" rx=\"15\" fill=\"#fff\"/>\n", 0.0, 0.0, out_size, out_size);
+
+        //fprintf(svg, "</svg>\n");
+        //fclose(svg);
+    }
 
     // Process points
     IDelaBella2 < double > * idb = IDelaBella2 < double > ::Create();
@@ -87,6 +132,7 @@ int main( int argc, char* argv[] )
 
     printf( "Triangulate\n" );
 
+    int tris = 0;
     int verts = idb->Triangulate( npt, &cloud->x, &cloud->y, sizeof( dba_point ) );
 
     if ( verts > 0 )
@@ -103,7 +149,7 @@ int main( int argc, char* argv[] )
         assert(polys == idb->Polygonize());
         */
 
-        int tris = idb->FloodFill(false, 0);      
+        tris = idb->FloodFill(false, 0);      
 
         printf( "Done\n" );
   
@@ -177,11 +223,28 @@ int main( int argc, char* argv[] )
         if (o)
             fprintf(o,"%d\n", tris);
 
+
+        int pal[6][3] = 
+        {
+            {255,0,0}, {255,255,0}, {0,255,0},
+            {0,255,255}, {0,0,255}, {255,0,255},
+        };
+
         for (int i=0; i<tris; i++)
         {
             printf("%d %d %d %d\n", i, t[i].v[0],t[i].v[1],t[i].v[2]);
             if (o)
                 fprintf(o,"%d %d %d %d\n", i, t[i].v[0],t[i].v[1],t[i].v[2]);
+
+            if (svg)
+            {
+                fprintf(svg, "  <path stroke-width=\"0.25\" fill=\"RGB(%d,%d,%d)\" fill-opacity=\"0.25\" d=\"", pal[i%6][0], pal[i%6][1], pal[i%6][2]);
+                fprintf(svg, "M %f %f L %f %f  L %f %f  Z", 
+                    cloud[t[i].v[0]].x*svg_scale + svg_trans_x, cloud[t[i].v[0]].y*svg_scale + svg_trans_y,
+                    cloud[t[i].v[1]].x*svg_scale + svg_trans_x, cloud[t[i].v[1]].y*svg_scale + svg_trans_y,
+                    cloud[t[i].v[2]].x*svg_scale + svg_trans_x, cloud[t[i].v[2]].y*svg_scale + svg_trans_y);
+                fprintf(svg, "\" />\n");
+            }
         }
 
         if (o)
@@ -190,6 +253,39 @@ int main( int argc, char* argv[] )
     else
     {
         printf( "DLB Error! %d\n", verts );
+    }
+
+    if (svg)
+    {
+        // overlay input
+        for (int j=0; j<npt; j++)
+        {
+            fprintf(svg, "  <circle cx=\"%f\" cy=\"%f\" r=\"3\" />\n", cloud[j].x*svg_scale + svg_trans_x, cloud[j].y*svg_scale + svg_trans_y);
+        }
+
+        if (nedg)
+        {
+            fprintf(svg, "  <path stroke-width=\"2\" d=\"");
+            for (int i=0; i<nedg; i++)
+            {
+                fprintf(svg, "M %f %f L %f %f ", 
+                    cloud[bounds[i].a].x*svg_scale + svg_trans_x, cloud[bounds[i].a].y*svg_scale + svg_trans_y,
+                    cloud[bounds[i].b].x*svg_scale + svg_trans_x, cloud[bounds[i].b].y*svg_scale + svg_trans_y);
+            }
+
+            fprintf(svg, "\" />\n");
+        }
+
+        for (int j=0; j<npt; j++)
+        {
+            fprintf(svg, "  <text stroke=\"none\" fill=\"#800\" font-size=\"16\" font-family=\"Arial, Helvetica, sans-serif\" x=\"%f\" y=\"%f\">%d</text>\n", cloud[j].x*svg_scale + svg_trans_x - 5, cloud[j].y*svg_scale + svg_trans_y - 4, j);
+        }
+
+
+        fprintf(svg, "  <text stroke=\"none\" fill=\"#000\" font-size=\"24\" font-family=\"Arial, Helvetica, sans-serif\" x=\"%f\" y=\"%f\">%d %s</text>\n", 24.0, 24.0, tris, "triangles");
+
+        fprintf(svg, "</svg>\n");
+        fclose(svg);        
     }
 
     delete[] cloud;
